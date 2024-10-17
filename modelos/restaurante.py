589cb3f5a26a63ccad320362af894fc36b22d5b0
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-10-16 23:42:56 -0300

    add restaurante class

---
+++ modelos/restaurante.py
@@ -0,0 +1,17 @@
+class Restaurante:
+    nome: str
+    categoria: str
+    ativo: bool
+
+    def __init__(self, nome: str, categoria: str):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+
+    def __str__(self):
+        return f"{self.nome} | {self.categoria}"
+
+
+restaurante_praca = Restaurante("Praça", "italiano")
+
+print(restaurante_praca)
